09c8084fe1a98da55e392f2bc89c6f0c17a9b1f7
Michal Dvorak <mikee2185@gmail.com>
2020-10-15 19:13:24 +0200

    Revert "back to text-based config"
    Simplified smart config

---
+++ init-config.sh
@@ -0,0 +1,19 @@
+out="/etc/oauth2-proxy/oauth2-proxy.cfg"
+
+for f in /source/config/* ; do
+  if [ "$(basename "$f")" != "__authenticated_emails" ]; then
+    echo "$(basename "$f") = $(cat "$f")" >> "$out"
+  else
+    # NOTE hack-ish way to support inline authenticated email list in config
+    cat "$f" > /etc/oauth2-proxy/authenticated-emails.txt
+    echo 'authenticated_emails_file = "/etc/oauth2-proxy/authenticated-emails.txt"' >> "$out"
+  fi
+done
+
+for f in /source/secret/* ; do
+  # quote values as strings
+  echo "$(basename "$f") = \"$(cat "$f")\"" >> "$out"
+done
+
+# TODO debug
+cat "$out"
